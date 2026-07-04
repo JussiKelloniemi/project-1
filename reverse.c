b6e789e3af9b98fb99db459aa9585e863cf849f7
Jussi Kelloniemi <jussi.kelloniemi@student.lut.fi>
2026-07-04 21:27:00 +0300

    Corrections to error messages and codes, writeFile function added

--- reverse.c
+++ reverse.c
@@ -15,7 +15,7 @@ Node *createNode(char *line) {
         printf("malloc failed");
         exit(1);
     }
-    if ((pNewNode->line = (Node*)malloc(strlen(line) + 1)) == NULL) {
+    if ((pNewNode->line = (char*)malloc(strlen(line) + 1)) == NULL) {
         printf("malloc failed");
         exit(1);
     }
@@ -25,13 +25,13 @@ Node *createNode(char *line) {
     return(pNewNode);
 }
 
-void addNode(Node** pHead, char *line) {
+Node* addNode(Node** pHead, char *line) {
     
     Node* newNode = createNode(line); 
 
     if (*pHead == NULL) {
         *pHead = newNode;
-        return;
+        return(newNode);
     }
 
     Node* pCurrent = *pHead;
@@ -43,66 +43,96 @@ void addNode(Node** pHead, char *line) {
     pCurrent->next = newNode;
     newNode->prev = pCurrent;
     
-    return;
+    return(newNode);
 }
 
 // Reads input file and stores the contets of that file
-void readFile(const char *inputFile, Node** pHead) {
+Node* readFile(const char *inputFile, Node** pHead) {
     char *line = NULL;
+    Node *pTail = NULL;
     size_t len = 0;
 
     FILE *fp = fopen(inputFile, "r");
     if (fp == NULL) {
-        printf("error: cannot open file '%s'", inputFile);
+        fprintf(stderr, "reverse: cannot open file '%s'\n", inputFile);
         exit(1);
     }
     
     while (getline(&line, &len, fp) != -1) {
-        addNode(pHead, line);
+        line[strcspn(line, "\n")] = '\n';
+        pTail = addNode(pHead, line);
     }
     free(line);
     fclose(fp);
+    return(pTail);
 }
 
 
 
 // Writes output file whenever one is given
-void writeFile() {
+void writeFile(const char *outputFile, Node* pTail) {
+    FILE *fp = fopen(outputFile, "w");
+    if (fp == NULL) {
+        fprintf(stderr, "error: cannot open file '%s'\n", outputFile);
+        exit(1);
+    }
 
+    Node* pCurrent = pTail;
+
+    while(pCurrent != NULL) {
+        fprintf(fp, "%s", pCurrent->line);
+        pCurrent = pCurrent->prev;
+    }
+    printf("\n");
+    return;
+    fclose(fp);
 }
 
 // Prints out input file reversed
-void printList(Node* head) {
+void printList(Node* pTail) {
     
-    Node* pCurrent = head;
+    Node* pCurrent = pTail;
     while(pCurrent != NULL) {
         printf("%s", pCurrent->line);
-        pCurrent = pCurrent->next;
+        pCurrent = pCurrent->prev;
     }
+    printf("\n");
     return;
 }
 
 int main(int argc, char *argv[]) {
     
     Node *pHead = NULL;
+    Node *pTail = NULL;
+
     if (argc > 3) {
-        printf("usage: reverse <input> <output>\n");
-        return(1);
+        fprintf(stderr, "usage: reverse <input> <output>\n");
+        exit(1);
     }
 
     
 
     if (argc == 2) {
         const char *inputName = argv[1];
-        readFile(inputName, &pHead);
+        pTail = readFile(inputName, &pHead);
         //addNode(pHead);
-        printList(pHead);
+        printList(pTail);
 
         //printf("test 1 argument");
     }
 
     if (argc == 3) {
-        printf("test 2 argument");
+        const char *inputName = argv[1];
+        const char *outputName = argv[2];
+
+        if(strcmp(inputName, outputName) == 0) {
+            fprintf(stderr, "reverse: input and output file must differ\n");
+            exit(1);
+        }
+        pTail = readFile(inputName, &pHead);
+        writeFile(outputName, pTail);
+        
+        //printf("test 2 argument");
     }
     
 
